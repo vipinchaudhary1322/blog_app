29b708706c6bdabaa8a0255dc342f851765320ab
Vipin Chaudhary <108751242+vipinchaudhary1322@users.noreply.github.com>
2022-12-28 18:56:18 +0530

    MERN

---
+++ Blog_App_Mern/frontend/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+      
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://bloggerspassion.com/wp-content/uploads/2020/03/Best-Blogging-Apps.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
